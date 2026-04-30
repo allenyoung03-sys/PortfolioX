f43ed935504428dab115fd2aa734c16d095b5911
allenyoung03-sys <allenyoung03@gmail.com>
2026-04-30 13:52:19 +0800

    Initial Commit

---
+++ portfoliox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  portfoliox
+//
+//  Created by Yang Yang on 2026/4/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ portfoliox/portfolioxApp.swift
@@ -0,0 +1,17 @@
+//
+//  portfolioxApp.swift
+//  portfoliox
+//
+//  Created by Yang Yang on 2026/4/30.
+//
+
+import SwiftUI
+
+@main
+struct portfolioxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
